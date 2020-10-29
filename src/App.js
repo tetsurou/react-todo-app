3653b88812f1d9effc4745e2b5757d5391de0a09
Tetsuro Uno <ttrun5706@gmail.com>
2020-10-30 08:30:34 +0900

    feat: 追加処理の作成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import TodoList from './TodoList';
+import Form from './Form';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            list: ['プログラミング','お買い物'],
+            value: '',
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            list:[
+                ...this.state.list,
+                this.state.value,
+            ],
+            value: '',
+        });
+    }
+
     render() {
-        const list = ['プログラミング','お買い物'];
         return (
             <div>
                 <h1>TODOアプリ</h1>
-                <TodoList list={list}/>
+                <Form 
+                    value={this.state.value}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                />
+                <TodoList list={this.state.list}/>
             </div>
         );
     }
---
+++ src/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+function Form(props) {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <TextField
+                name="value"
+                label="やること"
+                value={props.value}
+                onChange={props.handleChange}
+            />
+            <Button
+                type="submit"
+                variant="outlined"
+                color="primary"
+            >
+                追加
+            </Button>
+        </form>
+    )
+}
+
+export default Form;
